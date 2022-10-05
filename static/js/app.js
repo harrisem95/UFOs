0333e85ed7f24ee096e9aec1908a5d4cb739e5af
Emily Harris <emily@Chriss-Air.lan>
2022-10-05 07:32:01 -0400

    new changes

--- static/js/app.js
+++ static/js/app.js
@@ -45,10 +45,9 @@ function handleClick() {
     //@NOTE: if no date was entered then filteredData will
     //just be the original tableData.
     buildTable(filteredData);
-}
 
 //Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 //Build the table when the page loads
-buildTable(tableData);
+buildTable(tableData);}
